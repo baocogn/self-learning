badaa910fcc054b767c05674fcda44771fbcef12
Ngoc <baocogn123@gmail.com>
2018-08-03 20:08:09 +0700

    add day 4 quiz 1

--- big_o_coding/day_4_quiz_1.py
+++ big_o_coding/day_4_quiz_1.py
@@ -1,30 +1,27 @@
+def isPrime(x):
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
 n = int(input())
 n1 = n
 n2 = n
-found1 = False
-found2 = True
 
-while found1 == False:
-    flag1 = True
-    for i in range(2, int(n1 ** 0.5) + 1):
-        if n1 % i == 0:
-            flag1 = False
-if flag1 == True:
-    found1 = True
-else:
-    n1 -= 1
+while True:
+    if isPrime(n1):
+        break
+    else:
+        n1 -= 1
 
-while found2 == False:
-    flag2 = True
-    for i in range(2, int(n2 ** 0.5) + 1):
-        if n2 % i == 0:
-            flag2 = False
-if flag2 == True:
-    found2 = True
-else:
-    n2 += 1
+while True:
+    if isPrime(n2):
+        break
+    else: 
+        n2 += 1
 
-if n - n1 <= n - n2:
+if n - n1 <= n2 - n:
     print(n1)
 else:
-    print(n2)    
+    print(n2)
+            
